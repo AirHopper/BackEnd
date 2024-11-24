77821689fb92b1b283cb65d52484be4819a166cc
MRoyhanF <froyhan0@gmail.com>
2024-11-25 00:37:08 +0700

    feat: flight | setup pagination - Not finished

--- src/controllers/flightController.js
+++ src/controllers/flightController.js
@@ -1,22 +1,19 @@
 import { getAllFlights } from "../services/flightServices.js";
-import AppError from "../utils/AppError.js";
 
 // Get all flights
-export const getAll = async (req, res) => {
+export const getAll = async (req, res, next) => {
   try {
-    const flights = await getAllFlights();
+    const {
+      flights,
+      // pagination
+    } = await getAllFlights();
     res.status(200).json({
       success: true,
       message: "Flights fetched successfully",
       data: flights,
+      // pagination,
     });
-  } catch (err) {
-    const statusCode = err instanceof AppError ? err.statusCode : 500;
-    const message = err instanceof AppError ? err.message : "Internal Server Error";
-
-    res.status(statusCode).json({
-      success: false,
-      message: message,
-    });
+  } catch (error) {
+    next(error);
   }
 };
--- src/services/flightServices.js
+++ src/services/flightServices.js
@@ -2,82 +2,115 @@
 import AppError from "../utils/AppError.js";
 import { formattedFlightsDummy } from "../dummy/flight.dummy.js";
 
-export const getAllFlights = async (req, res) => {
+// export const getAllFlights = async (req, res) => {
+//   try {
+//     // const flights = await prisma.flight.findMany({
+//     //   include: {
+//     //     Route: {
+//     //       include: {
+//     //         DepartureAirport: true,
+//     //         ArrivalAirport: true,
+//     //       },
+//     //     },
+//     //     Airplane: {
+//     //       include: {
+//     //         Airline: true,
+//     //       },
+//     //     },
+//     //     DepartureTerminal: true,
+//     //     ArrivalTerminal: true,
+//     //     Discount: true,
+//     //   },
+//     // });
+
+//     // if (!flights.length) {
+//     //   throw new AppError("No flights found", 404);
+//     // }
+
+//     // // Format response
+//     // const formattedFlights = flights.map((flight) => ({
+//     //   id: flight.id,
+//     //   class: flight.class,
+//     //   airline: flight.Airplane.Airline.name,
+//     //   airplane: flight.Airplane.name,
+//     //   departure: {
+//     //     place: flight.Route.DepartureAirport.city,
+//     //     airport: {
+//     //       name: flight.Route.DepartureAirport.name,
+//     //       code: flight.Route.DepartureAirport.iataCode,
+//     //     },
+//     //     city: {
+//     //       name: flight.Route.DepartureAirport.city,
+//     //       code: flight.Route.DepartureAirport.cityCode,
+//     //     },
+//     //     country: {
+//     //       name: flight.Route.DepartureAirport.country,
+//     //       code: flight.Route.DepartureAirport.countryCode,
+//     //     },
+//     //     time: flight.departureTime,
+//     //     terminal: flight.DepartureTerminal.name,
+//     //   },
+//     //   arrival: {
+//     //     place: flight.Route.ArrivalAirport.city,
+//     //     airport: {
+//     //       name: flight.Route.ArrivalAirport.name,
+//     //       code: flight.Route.ArrivalAirport.iataCode,
+//     //     },
+//     //     city: {
+//     //       name: flight.Route.ArrivalAirport.city,
+//     //       code: flight.Route.ArrivalAirport.cityCode,
+//     //     },
+//     //     country: {
+//     //       name: flight.Route.ArrivalAirport.country,
+//     //       code: flight.Route.ArrivalAirport.countryCode,
+//     //     },
+//     //     time: flight.arrivalTime,
+//     //     terminal: flight.ArrivalTerminal.name,
+//     //   },
+//     //   isActive: flight.isActive,
+//     //   price: flight.price,
+//     //   baggage: flight.baggage,
+//     //   cabinBaggage: flight.cabinBaggage,
+//     //   entertainment: flight.entertainment,
+//     //   discount: flight.Discount ? flight.Discount.discount : 0,
+//     // }));
+
+//     return formattedFlightsDummy;
+//   } catch (error) {
+//     console.error("Database Error:", error); // Log detail error
+//     throw new AppError("Database query failed", 500, error.message);
+//   }
+// };
+
+export const getAllFlights = async (req) => {
   try {
-    // const flights = await prisma.flight.findMany({
-    //   include: {
-    //     Route: {
-    //       include: {
-    //         DepartureAirport: true,
-    //         ArrivalAirport: true,
-    //       },
-    //     },
-    //     Airplane: {
-    //       include: {
-    //         Airline: true,
-    //       },
-    //     },
-    //     DepartureTerminal: true,
-    //     ArrivalTerminal: true,
-    //     Discount: true,
-    //   },
-    // });
+    // console.log("Query Params:", req.query);
 
-    // if (!flights.length) {
-    //   throw new AppError("No flights found", 404);
-    // }
+    // const page = parseInt(req.query.page) || 1;
+    // const limit = parseInt(req.query.limit) || 10;
 
-    // // Format response
-    // const formattedFlights = flights.map((flight) => ({
-    //   id: flight.id,
-    //   class: flight.class,
-    //   airline: flight.Airplane.Airline.name,
-    //   airplane: flight.Airplane.name,
-    //   departure: {
-    //     place: flight.Route.DepartureAirport.city,
-    //     airport: {
-    //       name: flight.Route.DepartureAirport.name,
-    //       code: flight.Route.DepartureAirport.iataCode,
-    //     },
-    //     city: {
-    //       name: flight.Route.DepartureAirport.city,
-    //       code: flight.Route.DepartureAirport.cityCode,
-    //     },
-    //     country: {
-    //       name: flight.Route.DepartureAirport.country,
-    //       code: flight.Route.DepartureAirport.countryCode,
-    //     },
-    //     time: flight.departureTime,
-    //     terminal: flight.DepartureTerminal.name,
-    //   },
-    //   arrival: {
-    //     place: flight.Route.ArrivalAirport.city,
-    //     airport: {
-    //       name: flight.Route.ArrivalAirport.name,
-    //       code: flight.Route.ArrivalAirport.iataCode,
-    //     },
-    //     city: {
-    //       name: flight.Route.ArrivalAirport.city,
-    //       code: flight.Route.ArrivalAirport.cityCode,
-    //     },
-    //     country: {
-    //       name: flight.Route.ArrivalAirport.country,
-    //       code: flight.Route.ArrivalAirport.countryCode,
-    //     },
-    //     time: flight.arrivalTime,
-    //     terminal: flight.ArrivalTerminal.name,
-    //   },
-    //   isActive: flight.isActive,
-    //   price: flight.price,
-    //   baggage: flight.baggage,
-    //   cabinBaggage: flight.cabinBaggage,
-    //   entertainment: flight.entertainment,
-    //   discount: flight.Discount ? flight.Discount.discount : 0,
-    // }));
+    // const startIndex = (page - 1) * limit;
+    // const endIndex = page * limit;
 
-    return formattedFlightsDummy;
+    // const flights = formattedFlightsDummy.slice(startIndex, endIndex);
+    const flights = formattedFlightsDummy;
+
+    // const totalItems = formattedFlightsDummy.length;
+    // const totalPages = Math.ceil(totalItems / limit);
+
+    // const paginationInfo = {
+    //   totalItems,
+    //   totalPages,
+    //   currentPage: page,
+    //   limit,
+    // };
+
+    return {
+      flights,
+      // pagination: paginationInfo,
+    };
   } catch (error) {
-    console.error("Database Error:", error); // Log detail error
-    throw new AppError("Database query failed", 500, error.message);
+    console.error("Error geting flight data:", error);
+    throw error;
   }
 };
