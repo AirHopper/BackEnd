ec50b32cd0b818f58651bfdaa009fb562142dc8c
MRoyhanF <froyhan0@gmail.com>
2024-12-02 11:34:21 +0700

    udpate: cd | update cd

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+});
 
 // await prisma.$connect();
 // console.log("Database connected");
