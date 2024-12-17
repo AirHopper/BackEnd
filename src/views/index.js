e45d9a7917ee1c92394f2113b6f6befd9b097075
MRoyhanF <froyhan0@gmail.com>
2024-12-17 09:00:37 +0700

    udpate head layout

--- src/views/index.js
+++ src/views/index.js
@@ -4,7 +4,7 @@
 export default (app) => {
   app.get("/", (req, res) => {
     res.render("home", {
-      title: "Home Page",
+      title: "Airhopper API",
       docs_url: `http://${process.env.APP_URL}/api/v1/api-docs`,
     });
   });
